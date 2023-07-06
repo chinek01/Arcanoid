1df1ecb22801d80b52341f3198ab6c1d351ecda6
MarcinC <chinek01@gmail.com>
2023-07-06 15:55:44 +0200

    board init test

--- board/board.py
+++ board/board.py
@@ -20,3 +20,20 @@ class Board(Turtle):
     def __init__(self):
         super().__init__()
 
+
+# some test
+if __name__ == '__main__':
+    # screen init
+    screen = Screen()
+    screen.title("Board test")
+    screen.setup(
+        width=800,
+        height=600
+    )
+    screen.bgcolor("#323232")
+    screen.tracer(0)
+
+    x = Board()
+
+    screen.update()
+    screen.exitonclick()

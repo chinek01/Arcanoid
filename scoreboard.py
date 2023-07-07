f91fdcfec31f8f726c278b708a4f3495b2fb927a
MarcinC <chinek01@gmail.com>
2023-07-07 15:44:52 +0200

    start test block

--- scoreboard.py
+++ scoreboard.py
@@ -20,3 +20,23 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+
+
+# some test
+if __name__ == '__main__':
+    # screen init
+    width = 800
+    height = 600
+    screen = Screen()
+    screen.title("Scoreboard test")
+    screen.setup(
+        width=width,
+        height=height
+    )
+    screen.bgcolor('#323232')
+    screen.tracer(0)
+
+    x = Scoreboard()
+
+    screen.update()
+    screen.exitonclick()

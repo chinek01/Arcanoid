6f78633c7b7f18e121d775fb98256306de01a015
MarcinC <chinek01@gmail.com>
2023-07-10 14:33:14 +0200

    init and supplement flag

--- game_core/game_core.py
+++ game_core/game_core.py
@@ -10,3 +10,27 @@ Date: 2023-07-06
 Author: MC
 
 """
+
+LIFE = 3
+
+
+class Game_core:
+
+    def __init__(self):
+        self.cur_life = LIFE
+        self.game_over_flag = False
+
+    def loose_life(self):
+        self.cur_life -= 1
+        self.check_life()
+
+    def check_life(self):
+        if self.cur_life == 0:
+            self.game_over_flag = True
+
+    def get_game_over_flag(self):
+        return self.game_over_flag
+
+    def reset(self):
+        self.cur_life = LIFE
+        self.game_over_flag = False

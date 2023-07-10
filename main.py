41c8e9d6eb500f19f5e1491f291aec15d0ee40c0
MarcinC <chinek01@gmail.com>
2023-07-10 20:45:20 +0200

    all together PART 01

--- main.py
+++ main.py
@@ -9,3 +9,32 @@ Author: MC
 """
 
 
+from turtle import Screen
+
+from scoreboard.scoreboard import Scoreboard
+from game_core.game_core import Game_core
+from board.board import Board
+from block.block import Block
+from ball.ball import Ball
+
+
+# Game options
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_BG_COLOR = "#323232"
+
+PLAY_SCREEN_WIDTH = 800
+PLAY_SCREEN_HEIGHT = SCREEN_HEIGHT - 100
+
+
+screen = Screen()
+screen.title("Arcanoid by MC")
+screen.setup(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT
+)
+screen.tracer(0)
+
+# init game core class
+game_core = Game_core()
+

a6edf41e1b3c7a88fe79e61060db651793e0dfb5
MarcinC <chinek01@gmail.com>
2023-07-10 20:58:45 +0200

    game core flag reverse status

--- game_core/game_core.py
+++ game_core/game_core.py
@@ -18,7 +18,7 @@ class Game_core:
 
     def __init__(self):
         self.cur_life = LIFE
-        self.game_over_flag = False
+        self.game_over_flag = True
 
     def loose_life(self):
         self.cur_life -= 1
@@ -26,11 +26,11 @@ class Game_core:
 
     def check_life(self):
         if self.cur_life == 0:
-            self.game_over_flag = True
+            self.game_over_flag = False
 
     def get_game_over_flag(self):
         return self.game_over_flag
 
     def reset(self):
         self.cur_life = LIFE
-        self.game_over_flag = False
+        self.game_over_flag = True
